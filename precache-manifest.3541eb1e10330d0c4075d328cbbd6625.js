fbb4379d8bcfdf1f3edaf50949c77215c0f71283
ricurdofh <ricardo.servitechs@gmail.com>
2020-01-17 15:53:17 -0400

    Updates

---
+++ precache-manifest.3541eb1e10330d0c4075d328cbbd6625.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a20e5a0ebc26986878c8c8f7a33bcff7",
+    "url": "/prueba-shopy/index.html"
+  },
+  {
+    "revision": "263a5644d1e2ea89d6e8",
+    "url": "/prueba-shopy/static/css/2.b90ce945.chunk.css"
+  },
+  {
+    "revision": "e28fea74f4252ef5d40d",
+    "url": "/prueba-shopy/static/css/main.95d678ff.chunk.css"
+  },
+  {
+    "revision": "263a5644d1e2ea89d6e8",
+    "url": "/prueba-shopy/static/js/2.60d56e2e.chunk.js"
+  },
+  {
+    "revision": "14cfafeab58f243640d9d112b6ecad16",
+    "url": "/prueba-shopy/static/js/2.60d56e2e.chunk.js.LICENSE"
+  },
+  {
+    "revision": "e28fea74f4252ef5d40d",
+    "url": "/prueba-shopy/static/js/main.f63e599d.chunk.js"
+  },
+  {
+    "revision": "635b912ef21ee22844f0",
+    "url": "/prueba-shopy/static/js/runtime-main.8b3be5a0.js"
+  },
+  {
+    "revision": "4893a5546c0277993681f06c15f51c80",
+    "url": "/prueba-shopy/static/media/star.4893a554.svg"
+  }
+]);
